43428641fd25e79e5f13a79b09a67db37f527785
Robert <109295306+rarellano5280@users.noreply.github.com>
2023-02-04 12:56:51 -0700

    refactored home routes to include post and comments from a user

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,13 +1,35 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
+router.get('/', (req, res) => {
   try {
-    res.render('homepage', {
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
+  Post.findAll({
+    attributes: [
+      'id',
+      'post_title',
+      'post_content',
+      'created_at'
+    ],
+    include: [
+      {
+          model: Comment,
+          attributes: ['id', 'comment_entry', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attribute: ['name']
+          }
+      },
+      {
+        model: User,
+        attributes: ['name']
+      }
+    ]
+  }).then(postData => {
+    const renderPosts = postData.map(post => post.get({ plain: true }));
+    res.render('homepage', {renderPosts, logged_in: req.session.logged_in});
+  }) 
+} catch (err) {
     res.status(500).json(err);
   }
 });
@@ -34,11 +56,54 @@ router.get('/profile', withAuth, async (req, res) => {
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/');
     return;
   }
 
   res.render('login');
 });
+
+router.get('/signup', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('signup');
+});
+
+router.get('/post/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'post_title',
+      'created_at',
+      'post_content'
+    ],
+    include: [
+      {
+          model: Comment,
+          attributes: ['id', 'comment_entry', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attribute: ['name']
+          }
+      },
+      {
+        model: User,
+        attributes: ['name']
+      }
+    ]
+  }) .then(postData => {
+    if(!postData) {
+      res.status(404).json({ message: 'Not able to find a post with that ID'})
+      return;
+    }
+  });
+});
+
 
 module.exports = router;
