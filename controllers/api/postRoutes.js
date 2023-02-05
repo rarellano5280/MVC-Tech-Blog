0b0259d4ee5301e568283aeaea9839f49318ffb5
Robert <109295306+rarellano5280@users.noreply.github.com>
2023-02-05 16:22:37 -0700

    added GET route to find all posts

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,4 +1,26 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
+const sequelize = require('../../config/connection');
+
+router.get('/', (req, res) => {
+  Post.findAll({
+    attributes: ['id', 'title', 'content', 'created_at'],
+    order: [['created_at', 'DESC']],
+    include: [
+      {
+        model: User,
+        attributes: ['name'],
+      },
+      {
+        model: Comment,
+        attributes: ['id', 'comment_input', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['name'],
+        },
+      },
+    ],
+  });
+});
 
 module.exports = router;
